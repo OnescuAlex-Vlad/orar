a8b0e79a534f3f285d2a67a29f8fa98080cc9781
OnescuAlex-Vlad <onescu8779@gmail.com>
2024-05-14 21:57:38 +0300

    Added a job run executioner

---
+++ runner.go
@@ -0,0 +1,29 @@
+package orar
+
+import (
+	"time"
+
+	"github.com/robfig/cron/v3"
+)
+
+type Func func()
+
+func Schedule(spec string, job cron.Job) error {
+	sched, err := cron.ParseStandard(spec)
+	if err != nil {
+		return err
+	}
+	MainCron.Schedule(sched, New(job))
+	return nil
+}
+
+
+func Every(duration time.Duration, job cron.Job) {
+
+	MainCron.Schedule(cron.Every(duration), New(job))
+}
+
+// Run the given job right now.
+func Now(job cron.Job) {
+	go New(job).Run()
+}
